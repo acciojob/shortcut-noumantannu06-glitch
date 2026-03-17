5df02abec292d5d7b1076a8a00c6a774d25e80e1
Md Nouman <noumantannu06@gmail.com>
2026-03-17 17:03:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,13 +7,11 @@ function shortcut(s1, s2) {
     const firstLetterStr2 = str2.charAt(0);
 	 return firstLetterStr1 + firstLetterStr2;
 }
+
 console.log(shortcut("Amnesty", "International")); 
 console.log(shortcut("Hello", "world"));            
-console.log(shortcut("apple", "banana"));           
-console.log(shortcut("Upper", "lower"));           
 console.log(shortcut("", "International"));          
 console.log(shortcut("Amnesty", ""));  
-
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
